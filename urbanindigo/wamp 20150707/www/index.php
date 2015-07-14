5e14a264e5017fdbb3e38f5a17af2a222c99792e
Tyler Hill <tylerhillwebdev@gmail.com>
2015-07-13 21:13:31 -0700

    mockups

---
+++ urbanindigo/wamp 20150707/www/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>tyler hill web development</title>
+		<link rel=stylesheet href=style.css />
+	</head>
+	<body>
+	<div id="cube">
+	<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="330px" height="330px" viewBox="0 0 330 330" enable-background="new 0 0 330 330" xml:space="preserve">
+<rect x="30" y="30" fill="#8F6A61" width="300" height="300"/>
+<rect x="20" y="20" fill="#BACEB0" width="300" height="300"/>
+<rect x="10" y="10" fill="#659990" width="300" height="300"/>
+<rect fill="#526767" width="300" height="300"/>
+<foreignObject width="114.526" height="26" x="10" y="10">
+<a href=portfolio.php>PORTFOLIO</a>
+</foreignObject>
+<foreignObject width=131.135 height=26 x=10 y=36>
+<a href=tylerhillresume.pdf >RESUME.PDF</a>
+</foreignObject>
+<foreignObject width=78.91 height=26 x=10 y=62>
+<a href=http://github.com/tylerhill >GITHUB</a>
+</foreignObject>
+<foreignObject width=280.8 height=86.7 x=10 y=117>
+<p class=copy>TYLER HILL LIVES IN OAKLAND, CA. HE WRITES CODE IN:</p>
+<p class=copy>HTML5, CSS, JAVASCRIPT, PHP, PYTHON, JAVA, AND C.</p>
+</foreignObject>
+<foreignObject width=258 height=45 x=32 y=249>
+<p class=contact>(650) 787-8764</p>
+<p class=contact>TYLERHILLWEBDEV@GMAIL.COM</p>
+</foreignObject>
+</svg>	
+</div>
+		<div id=footer>
+			<p id=tyler>tyler</p>
+			<p id=hill>hill</p>
+			<p id=web>web</p>
+			<p id=dev>development</p>
+		</div>
+	</body>
+</html>
